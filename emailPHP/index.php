18189ab36f6f615527ccb284709a7c2efd0525d5
lucas matos <lucasmatoss2000@gmail.com>
2019-06-15 19:31:16 -0300

    envio de email utilizando PHP

---
+++ emailPHP/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv=”content-type” content="text/html;" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="keywords" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>site Fatech</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body style="background: #e3e3e3;">
+    <nav id="links-menu" style="background: gray;" class="collapse navbar-collapse">
+
+      <ul class="nav navbar-nav navbar-left">
+        <li><a  style="color: #000" href="index.php">Home</a></li>
+        <li><a  style="color: #000" href="cadastro.php">Enviar E-mail</a></li>
+      </ul>
+    </nav>
+  </body>
+</html>
+
